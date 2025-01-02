7d21bceb30e0ecb2f6d9225f20d9e97678819346
Himanshu0325 <himanshubidwaik79@gmail.com>
2025-01-02 16:35:53 +0530

    Password checking and Access ,Refresh Tokens

--- Backend/src/Models/user.model.js
+++ Backend/src/Models/user.model.js
@@ -49,4 +49,43 @@ const userSchema = new Schema(
   }
 )
 
+userSchema.pre("save", async function (next) {
+  if(! this.isModified(this.password))
+  { return
+    next()
+  }
+
+  this.password = bcrypt.hash(this.password)
+  next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password){
+  return await bcrypt.compare("password" , this.password)
+}
+
+userSchema.methods.generateAccessToken = async function(){
+  return jwt.sign(
+    { 
+      _id : this._id,
+      username : this.username,
+      fullname : this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+userSchema.methods.generateRefreshToken = async function () {
+  return jwt.sign(
+    { 
+      _id : this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
 export const user = mongoose.model("User",userSchema)
